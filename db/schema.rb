cd44fcb6901dbc407e58360f84b9216da33d3728
aem <amarkon895@gmail.com>
2015-11-01 13:13:45 -0500

    Issue #6: Created user_pictures table w/o FKs or join columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101175631) do
+ActiveRecord::Schema.define(version: 20151101180725) do
 
   create_table "menu_items", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 20151101175631) do
     t.text     "description", limit: 65535
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
+  end
+
+  create_table "user_pictures", force: :cascade do |t|
+    t.string   "picture_url", limit: 255, null: false
+    t.datetime "created_at",              null: false
   end
 
 end
