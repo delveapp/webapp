2c421c1c847679b20bca5683aa37de0579da25ee
aem <amarkon895@gmail.com>
2015-11-01 12:49:10 -0500

    Issue #3: Added basic menu_item table (no FKs/join columns)

---
+++ db/migrate/20151101164736_create_menu_item.rb
@@ -0,0 +1,10 @@
+class CreateMenuItem < ActiveRecord::Migration
+  def change
+    create_table :menu_items do |t|
+      t.string :name
+      t.string :picture_url
+      t.decimal :price, :precision => 4, :scale => 2
+      t.timestamps null:false
+    end
+  end
+end
