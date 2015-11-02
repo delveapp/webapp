199dc15767153351844ac592a80409917cd7bdbf
aem <amarkon895@gmail.com>
2015-11-01 21:10:23 -0500

    Added menu_item_scores table w/o FKs and join columns (#5)

---
+++ db/migrate/20151102020116_create_menu_item_score.rb
@@ -0,0 +1,8 @@
+class CreateMenuItemScore < ActiveRecord::Migration
+  def change
+    create_table :menu_item_scores do |t|
+      t.decimal :score, :precision => 3, :scale => 2, null:false
+      t.timestamp :updated_at, null:false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101181601) do
+ActiveRecord::Schema.define(version: 20151102020116) do
 
   create_table "categories", force: :cascade do |t|
     t.string "category", limit: 255, null: false
+  end
+
+  create_table "menu_item_scores", force: :cascade do |t|
+    t.decimal  "score",      precision: 3, scale: 2, null: false
+    t.datetime "updated_at",                         null: false
   end
 
   create_table "menu_items", force: :cascade do |t|
@@ -39,5 +44,23 @@ ActiveRecord::Schema.define(version: 20151101181601) do
     t.string   "picture_url", limit: 255, null: false
     t.datetime "created_at",              null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
